d7665637640c554bf187f2488f7a9e8b9b250a11
daych12 <73504876+daych12@users.noreply.github.com>
2020-12-09 16:47:57 -0600

    Add files via upload
    Basic_javascript

---
+++ Basic_Javascript_1.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>title</title>
+	<meta charset="utf-8">
+</head>
+	<body>
+		<button onclick="My_First_Function()">
+		Click me!
+	</button>
+	<p id="Irish"></p>
+<script>
+	function My_First_Function() {
+		var String="Kiss me, I'm Irish";
+		var result= string.fontcolor("green");
+		document.getElementBYID("Irish").inner HTML=
+		result;
+	}
+</script>
+		
+	</body>
+</html>
